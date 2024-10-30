da48bbe94123a5473592cc5721eb041dc9c64128
Thomas Quinn <tquinn2135@gmail.com>
2024-10-29 18:26:47 -0700

    Numbers To roman numeral Converter

---
+++ RomanNumeralConverter/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Roman;
+
+
+namespace RomanNumeralConverter;
+
+public partial class MainPage : ContentPage
+{
+    private Converter _converter;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        _converter = new Converter();
+    }
+
+
+    private void Convert_OnClicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(TxtEntry.Text, out int number) && number >= 1)
+        {
+            string romanNumeral = _converter.NumberToRoman(number);
+            lblRoman.Text = romanNumeral;
+        }
+        else
+        {
+            lblRoman.Text = "Invalid Entry. Please Try Again!";
+        }
+    }
+
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        TxtEntry.Text = "";
+        lblRoman.Text = "";
+    }
+}
---
+++ RomanNumeralConverter/RomanToCardinal.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralConverter;
+
+public partial class RomanToCardinal : ContentPage
+{
+    public RomanToCardinal()
+    {
+        InitializeComponent();
+    }
+}
